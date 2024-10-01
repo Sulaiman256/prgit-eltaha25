0310b0cfcf150436e2dd485ebd3e9b9b0efb6f64
Sulaiman256 <eltahasantossuleiman@fpllefia.com>
2024-10-01 15:58:34 +0200

    resolver conflictos

--- mitja.c
+++ mitja.c
@@ -6,18 +6,18 @@ int main() {
 	float nou_valor;
 	int numero_repeticions=0;
 	char consulta='s';
-	
+
 	while(consulta=='s') {
 		printf("ElTaha25, Dòna la següent dada: ");
 		scanf("%f",&nou_valor);
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
 		printf("ElTaha25, Vols continuar (s/n): ");
-	 	scanf("%s",&consulta);.
+	 	scanf("%s",&consulta);
 
 	}
 	mitja=suma/numero_repeticions;
-	printf("ElTaha25, la mitja és: %.2f\n",mitja);	
+	printf("ElTaha25, la mitja és: %.2f\n",mitja);
 	return 0;
 }
 
